65d8fd27a6135f5f4c85be82e55b5544fc13bed2
Zia Uddin Chowdhury <rifatxia9@gmail.com>
2023-09-09 14:51:30 +0600

    2020 and 2021 prices data updated

---
+++ price_extracter.py
@@ -0,0 +1,48 @@
+import os
+import pdfplumber
+import pandas as pd
+
+def extract_and_save_pdf_page(pdf_path, output_folder, start_page, end_page):
+    with pdfplumber.open(pdf_path) as pdf:
+        for i in range(start_page - 1, end_page):
+            page = pdf.pages[i]
+            # Assuming there's only one table per page
+            table = page.extract_tables()[0]
+
+            # Convert extracted table to a DataFrame
+            df = pd.DataFrame(table[1:], columns=table[0])
+
+            # Save DataFrame as CSV
+            csv_filename = f"{output_folder}/page_{i + 1}.csv"
+            df.to_csv(csv_filename, index=False)
+
+
+def merge_csv_files(input_folder, output_filename):
+    all_data = pd.DataFrame()
+    for filename in os.listdir(input_folder):
+        if filename.endswith(".csv"):
+            csv_path = os.path.join(input_folder, filename)
+            df = pd.read_csv(csv_path)
+            all_data = all_data.append(df, ignore_index=True)
+
+    all_data.to_csv(output_filename, index=False)
+
+
+# Specify the input PDF file and output folder
+year = 2021
+part = 'prices'
+input_pdf_path = f'pdf_datasets\\{year}.pdf'
+output_folder = f'output/{year}'
+start_page = 635
+end_page = start_page + 6
+
+
+# Create the output folder if it doesn't exist
+os.makedirs(output_folder, exist_ok=True)
+
+# Split the PDF into separate pages and save tables as CSV
+extract_and_save_pdf_page(input_pdf_path, output_folder, start_page, end_page)
+
+# Merge CSV files
+output_filename = f'output\\{year}\\{part}.csv'
+merge_csv_files(output_folder, output_filename)
